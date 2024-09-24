9e33feda4cfd0fa86c01e57cc06f786a26a38d93
Ahmed Zahir <xahiru@gmail.com>
2024-09-24 22:10:38 +0800

    answer to p1_q6

---
+++ practical_one/q_6c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+    // Taking input
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    // Swap logic
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    // Output the result
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
