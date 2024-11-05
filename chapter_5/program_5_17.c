0023f7ee2ae298436744081a827d2e32388b2cf1
Ahmed Zahir <xahiru@gmail.com>
2024-11-05 23:50:17 +0800

    program_5_17 added

---
+++ chapter_5/program_5_17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    /* This program displays a table of numbers, their squares, and cubes */
+    /* starting from the number 1. The final number in the table is input by the user */
+
+    int num, final;
+
+    printf("Enter the final number for the table: ");
+    scanf("%d", &final);
+
+    printf("\nNumber   Square   Cube\n");
+    printf("-----------------------\n");
+
+    for (num = 1; num <= final; num++) {
+        printf("%3d      %4d     %5d\n", num, num * num, num * num * num);
+    }
+
+    return 0;
+}
