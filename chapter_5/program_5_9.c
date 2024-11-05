c95946f29d93ba9baa528f1598de287e6ef17aff
Ahmed Zahir <xahiru@gmail.com>
2024-11-05 23:35:52 +0800

    p5_9 added

---
+++ chapter_5/program_5_9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float grade;
+    float total = 0.01; // Note the initialization here
+
+    printf("\nTo stop entering grades, press ctrl + Z on Windows or ctrl + D on UNIX operating systems.\n\n");
+    printf("Enter a grade: ");
+
+    // Loop to read grades until EOF
+    while (scanf("%f", &grade) != EOF) {
+        total += grade;
+        printf("Enter a grade: ");
+    }
+
+    printf("\nThe total of the grades is %.2f\n", total);
+    return 0;
+}
