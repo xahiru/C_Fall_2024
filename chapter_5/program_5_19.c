701d3d1ab29cdaba3dff640797f3efce32b41ece
Ahmed Zahir <xahiru@gmail.com>
2024-11-20 12:43:35 +0800

    slight changes to print

--- chapter_5/program_5_19.c
+++ chapter_5/program_5_19.c
@@ -5,11 +5,11 @@ int main() {
 
     // Start of outer loop
     for (i = 1; i <= 5; i++) {
-        printf("i is now %d\n", i);
+        printf("\ni is now %d\n", i);
 
         // Start of inner loop
         for (j = 1; j <= 4; j++) {
-            printf("j = %d\n", j);
+            printf(" j = %d", j);
         }
         // End of inner loop
     }
