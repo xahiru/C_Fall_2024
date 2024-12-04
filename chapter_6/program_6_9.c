522cb354f308e91958b0e90d4d3df5c9da6265d8
Ahmed Zahir <xahiru@gmail.com>
2024-12-04 11:46:36 +0800

    improved random

--- chapter_6/program_6_9.c
+++ chapter_6/program_6_9.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-int flip(int); /* prototype for flip function */
-void percentages(int, int);
+int flip(int numTimes); /* prototype for flip function */
+void percentages(int numTosses, int heads);
+
 int main()
 {
-    int numTosses = 1000;
+    int numTosses = 1000; // Number of tosses
     int heads;
 
+    srand(time(NULL)); // Initialize random number generator
     heads = flip(numTosses);
     percentages(numTosses, heads);
     return 0;
 }
 
-// this method tosses the coin numTimes
-// and returns the number of heads
+// Tosses the coin numTimes and returns the number of heads
 int flip(int numTimes)
 {
     int randValue;
     int heads = 0;
-    int i;
-    srand(time(NULL));
-    for (i = 1; i <= numTimes; i++)
+
+    for (int i = 1; i <= numTimes; i++)
     {
-        randValue = 1 + (int)rand() % 100;
-        if (randValue > 50)
+        randValue = rand() % 2; // 0 for tails, 1 for heads
+        if (randValue == 1)     // Increment heads count if it's 1
             heads++;
     }
-    return (heads);
+    return heads;
 }
 
-// this method calculates and displays
-// the percentages of heads and tails
+// Calculates and displays the percentages of heads and tails
 void percentages(int numTosses, int heads)
 {
-    int tails;
-    float perheads, pertails;
+    int tails = numTosses - heads;
+    float perheads = (float)heads / numTosses * 100.0;
+    float pertails = (float)tails / numTosses * 100.0;
+
     if (numTosses == 0)
+    {
         printf("There were no tosses, so no percentages can be calculated.\n");
+    }
     else
     {
-        tails = numTosses - heads;
-        printf("Number of coin tosses: $d\n", numTosses);
-        printf("Heads:sdTails: gd\n", heads, tails);
-        perheads = (float)heads / numTosses * 100.0;
-        pertails = (float)(numTosses - heads) / numTosses * 100.0;
-        printf("Heads came up $6.2f percent of the time.\n", perheads);
-        printf("Tails came up $6.2f percent of the time.\n", pertails);
+        printf("Number of coin tosses: %d\n", numTosses);
+        printf("Heads: %d Tails: %d\n", heads, tails);
+        printf("Heads came up %.2f percent of the time.\n", perheads);
+        printf("Tails came up %.2f percent of the time.\n", pertails);
     }
 }
