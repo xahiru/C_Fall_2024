6a546f0b17d10a947d8b3004dbc34ee4b1ca1507
Ahmed Zahir <xahiru@gmail.com>
2024-12-18 13:31:21 +0800

    testing again

--- chapter_9/exe1.c
+++ chapter_9/exe1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main() {
-    char s1[10] = {'S', 'e', 't', '\0', 'u', 'p', '\0'};
+int main(){
+    char s1[] = {'S', 'e', 't', '\0', 'u', 'p', '\0'};
     printf("%s", s1);
     return 0;
 }
